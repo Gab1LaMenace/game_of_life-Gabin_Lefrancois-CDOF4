63e8e6409724eb20b6e50f187c27d1084d58abd5
Gabin21 <gabin.lefrancois@gmail.com>
2025-01-13 17:31:15 +0100

    add of main and setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+    name='game_of_life-Gabin_Lefrancois-CDOF4',
+    version='1.0',
+    author='Gab1LaMenace',
+    license='MIT',
+    long_description=open('README.md').read(),
+)
